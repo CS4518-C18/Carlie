773d0974a8ea97ffc6e05797b1db42b6cc7ef8ae
Haofan Zhang <zhanghaofan999@gmail.com>
2018-02-16 23:27:52 -0500

    activity xml and navigation in manifest

--- app/src/main/java/com/harryliu/carlie/activities/passengerActivities/RequestTripActivity.java
+++ app/src/main/java/com/harryliu/carlie/activities/passengerActivities/RequestTripActivity.java
@@ -1,11 +1,21 @@
 package com.harryliu.carlie.activities.passengerActivities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.harryliu.carlie.R;
+
 /**
  * @author Harry Liu
  *
  * @version Feb 16, 2018
  */
 
-public class RequestTripActivity {
-
+public class RequestTripActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_trip);
+    }
 }
